498fd0bf43e39b2d052160bb4c9afd1c4319aab8
sumon <18103159@iubat.edu>
2024-01-13 14:07:59 +0600

    user profile blood group add

--- resources/views/Web/User/edit.blade.php
+++ resources/views/Web/User/edit.blade.php
@@ -25,6 +25,10 @@
                                     <input type="text" name="phone" class="form-control rounded-left" value="{{ $user->phone }}">
                                 </div>
                                 <div class="form-group">
+                                    <label for="Blood group" class="block mb-2 font-medium text-gray-900 dark:text-white">Blood group</label>
+                                    <input type="text" name="blood_group" class="form-control rounded-left" value="{{ $user->blood_group }}">
+                                </div>
+                                <div class="form-group">
                                     <label for="image" class="block mb-2 font-medium text-gray-900 dark:text-white">Image</label>
                                     <input type="file" class="form-control" name="image" id="image" aria-describedby="image-explanation">
                                 </div>
